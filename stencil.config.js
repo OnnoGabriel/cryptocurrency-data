8ce3b1a32a36c622183ea4ace2a1c98b04885cc4
Onno Gabriel <contact@datacodedesign.de>
2017-10-01 14:51:31 +0200

    Bugfix

--- stencil.config.js
+++ stencil.config.js
@@ -1,15 +1,10 @@
 exports.config = {
-  namespace: 'cryptocurrency-data',
-  generateDistribution: true,
-  generateWWW: false,
   bundles: [
     { components: ['cryptocurrency-data'] }
-  ],
-  collections: [{ name: '@stencil/router' }]
+  ]
 };
 
 exports.devServer = {
   root: 'www',
   watchGlob: '**/**'
 }
-HQvpyND77t8fYZDF6yVD
